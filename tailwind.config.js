c89c82fb64ebe76e0c34b883bebe0ca09f25a780
Yağızhan <hi@yagiz.dev>
2023-01-07 16:59:01 +0300

    This comment no longer applies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/Themes/Tailwind/Templates/*.{html,js}"], // Including the PHP files because some of the components are generated from PHP files
+  content: ["./src/Themes/Tailwind/Templates/*.{html,js}"],
   theme: {
     extend: {},
   },
