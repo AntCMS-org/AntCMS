5670529b11f543edd9bf8874628fe693ab7a0594
Yağızhan <hi@yagiz.dev>
2023-01-07 13:44:03 +0300

    Adjusting Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/Theme/Default/Templates/*.{html,js}", "./src/AntCMS/*.php"], // Including the PHP files because some of the components are generated from PHP files
+  content: ["./src/Themes/Tailwind/*.{html,js}", "./src/AntCMS/*.php"], // Including the PHP files because some of the components are generated from PHP files
   theme: {
     extend: {},
   },
