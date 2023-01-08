0c79ec0ba694adc2f1717fb3acbe6eefbab5b6bc
Yağızhan <hi@yagiz.dev>
2023-01-08 08:48:49 +0300

    Prose styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./src/Themes/Default/Templates/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      typography ({ theme }) {
+        return {
+          DEFAULT: {
+            css: {
+              'code::before': {
+                content: 'none',
+              },
+              'code::after': {
+                content: 'none'
+              },
+              code: {
+                backgroundColor: theme('colors.zinc.700'),
+                color: theme('colors.zinc.100'),
+                paddingLeft: theme('spacing[1.5]'),
+                paddingRight: theme('spacing[1.5]'),
+                paddingTop: theme('spacing.1'),
+                paddingBottom: theme('spacing.1'),
+                borderRadius: theme('borderRadius.DEFAULT'),
+              },
+            }
+          },
+        }
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
