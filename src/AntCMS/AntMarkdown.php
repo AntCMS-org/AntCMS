1dc25e2d32eda3789e8c4fc1d59dd893928f4bb8
Belle Aerni <belleaerni@gmail.com>
2023-01-08 22:01:05 -0800

    Move from php-markdown to commonmark
    Better maintained, much more popular, supports GH style markdown.
    Also added an emoji plugin for it so we don't have to maintain our own list of them.
    
    Unfortunately, moving this does nearly double the size of AntCMS, but it's a worthwhile tradeoff

--- src/AntCMS/AntMarkdown.php
+++ src/AntCMS/AntMarkdown.php
@@ -2,58 +2,18 @@
 
 namespace AntCMS;
 
-use Michelf\MarkdownExtra;
 use AntCMS\AntCache;
+use League\CommonMark\GithubFlavoredMarkdownConverter;
+use ElGigi\CommonMarkEmoji\EmojiExtension;
 
 class AntMarkdown
 {
-    const emojiMap = array(
-        ':smile:' => '😄',
-        ':grinning:' => '😀',
-        ':blush:' => '😊',
-        ':wink:' => '😉',
-        ':heart_eyes:' => '😍',
-        ':kissing_heart:' => '😘',
-        ':tongue:' => '😝',
-        ':stuck_out_tongue_winking_eye:' => '😜',
-        ':joy:' => '😂',
-        ':satisfied:' => '😌',
-        ':yum:' => '😋',
-        ':neutral_face:' => '😐',
-        ':expressionless:' => '😑',
-        ':unamused:' => '😒',
-        ':sweat_smile:' => '😅',
-        ':sweat:' => '😓',
-        ':pensive:' => '😔',
-        ':confused:' => '😕',
-        ':disappointed:' => '😞',
-        ':confounded:' => '😖',
-        ':fearful:' => '😨',
-        ':cold_sweat:' => '😰',
-        ':cry:' => '😢',
-        ':sob:' => '😭',
-        ':angry:' => '😠',
-        ':rage:' => '😡',
-        ':triumph:' => '😤',
-        ':sleepy:' => '😴',
-        ':dizzy_face:' => '😵',
-        ':mask:' => '😷',
-        ':scream:' => '😱',
-        ':flushed:' => '😳',
-        ':frowning:' => '😦',
-        ':anguished:' => '😧',
-        ':weary:' => '😩',
-        ':exploding_head:' => '🤯',
-        ':grimacing:' => '😬',
-        ':heart:' => '💓',
-        ':thumbsup:' => '👍',
-        ':thumbsdown:' => '👎'
-    );
-
     public static function renderMarkdown($md)
     {
         $cache = new AntCache();
         $cacheKey = $cache->createCacheKey($md, 'markdown');
+        $commonMark = new GithubFlavoredMarkdownConverter();
+        $commonMark->getEnvironment()->addExtension(new EmojiExtension());
 
         if ($cache->isCached($cacheKey)) {
             $cachedContent = $cache->getCache($cacheKey);
@@ -63,12 +23,7 @@ class AntMarkdown
             }
         }
 
-        $result = MarkdownExtra::defaultTransform($md);
-        $result = preg_replace('/(?:~~)([^~~]*)(?:~~)/', '<s>$1</s>', $result);
-
-        foreach (AntMarkdown::emojiMap as $markdown => $unicode) {
-            $result = str_replace($markdown, $unicode, $result);
-        }
+        $result = $commonMark->convert($md);
 
         $cache->setCache($cacheKey, $result);
         return $result;
