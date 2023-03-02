502f0a791e93882a94a6d8cdd4aeb2db8e79259a
Belle Aerni <belleaerni@gmail.com>
2023-03-01 20:27:03 -0800

    plugin/admin/ -> admin/

--- src/Plugins/Admin/AdminPlugin.php
+++ src/Plugins/Admin/AdminPlugin.php
@@ -82,13 +82,13 @@ class AdminPlugin extends AntPlugin
 
         switch ($route[0] ?? 'none') {
             case 'edit':
-                $HTMLTemplate = str_replace('<!--AntCMS-ActionURL-->', '//' . $currentConfig['baseURL'] . 'plugin/admin/config/save', $HTMLTemplate);
+                $HTMLTemplate = str_replace('<!--AntCMS-ActionURL-->', '//' . $currentConfig['baseURL'] . 'admin/config/save', $HTMLTemplate);
                 $HTMLTemplate = str_replace('<!--AntCMS-TextAreaContent-->', htmlspecialchars($currentConfigFile), $HTMLTemplate);
                 break;
 
             case 'save':
                 if (!$_POST['textarea']) {
-                    header('Location: //' . $currentConfig['baseURL'] . "plugin/admin/config/");
+                    header('Location: //' . $currentConfig['baseURL'] . "admin/config/");
                 }
 
                 $yaml = AntYaml::parseYaml($_POST['textarea']);
@@ -96,12 +96,12 @@ class AdminPlugin extends AntPlugin
                     AntYaml::saveFile(antConfigFile, $yaml);
                 }
 
-                header('Location: //' . $currentConfig['baseURL'] . "plugin/admin/config/");
+                header('Location: //' . $currentConfig['baseURL'] . "admin/config/");
                 exit;
 
             default:
                 $HTMLTemplate = "<h1>AntCMS Configuration</h1>\n";
-                $HTMLTemplate .= "<a href='//" . $currentConfig['baseURL'] . "plugin/admin/config/edit'>Click here to edit the config file</a><br>\n";
+                $HTMLTemplate .= "<a href='//" . $currentConfig['baseURL'] . "admin/config/edit'>Click here to edit the config file</a><br>\n";
                 $HTMLTemplate .= "<ul>\n";
                 foreach ($currentConfig as $key => $value) {
                     if (is_array($value)) {
@@ -146,7 +146,7 @@ class AdminPlugin extends AntPlugin
         switch ($route[0] ?? 'none') {
             case 'regenerate':
                 AntPages::generatePages();
-                header('Location: //' . AntConfig::currentConfig('baseURL') . "plugin/admin/pages/");
+                header('Location: //' . AntConfig::currentConfig('baseURL') . "admin/pages/");
                 exit;
 
             case 'edit':
@@ -169,7 +169,7 @@ class AdminPlugin extends AntPlugin
                     $page = "--AntCMS--\nTitle: New Page Title\nAuthor: Author\nDescription: Description of this page.\nKeywords: Keywords\n--AntCMS--\n";
                 }
 
-                $HTMLTemplate = str_replace('<!--AntCMS-ActionURL-->', '//' . AntConfig::currentConfig('baseURL') . "plugin/admin/pages/save/{$pagePath}", $HTMLTemplate);
+                $HTMLTemplate = str_replace('<!--AntCMS-ActionURL-->', '//' . AntConfig::currentConfig('baseURL') . "admin/pages/save/{$pagePath}", $HTMLTemplate);
                 $HTMLTemplate = str_replace('<!--AntCMS-TextAreaContent-->', htmlspecialchars($page), $HTMLTemplate);
                 break;
 
@@ -178,17 +178,17 @@ class AdminPlugin extends AntPlugin
                 $pagePath = AntTools::repairFilePath(antContentPath . '/' . implode('/', $route));
 
                 if (!isset($_POST['textarea'])) {
-                    header('Location: //' . AntConfig::currentConfig('baseURL') . "plugin/admin/pages/");
+                    header('Location: //' . AntConfig::currentConfig('baseURL') . "admin/pages/");
                 }
 
                 file_put_contents($pagePath, $_POST['textarea']);
-                header('Location: //' . AntConfig::currentConfig('baseURL') . "plugin/admin/pages/");
+                header('Location: //' . AntConfig::currentConfig('baseURL') . "admin/pages/");
                 exit;
 
             case 'create':
                 $HTMLTemplate = "<h1>Page Management</h1>\n";
                 $HTMLTemplate .= "<p>Create new page</p>\n";
-                $HTMLTemplate .= '<form method="post" action="' . '//' . AntConfig::currentConfig('baseURL') . 'plugin/admin/pages/edit">';
+                $HTMLTemplate .= '<form method="post" action="' . '//' . AntConfig::currentConfig('baseURL') . 'admin/pages/edit">';
                 $HTMLTemplate .=
                     '<div style="display:flex; flex-direction: row; justify-content: center; align-items: center">
                 <label for="input">URL for new page: ' . AntConfig::currentConfig('baseURL') . ' </label> <input type="text" name="newpage" id="input">
@@ -212,7 +212,7 @@ class AdminPlugin extends AntPlugin
                     AntYaml::saveFile(antPagesList, $pages);
                 }
 
-                header('Location: //' . AntConfig::currentConfig('baseURL') . "plugin/admin/pages/");
+                header('Location: //' . AntConfig::currentConfig('baseURL') . "admin/pages/");
                 break;
 
             case 'togglevisibility':
@@ -226,15 +226,15 @@ class AdminPlugin extends AntPlugin
                 }
 
                 AntYaml::saveFile(antPagesList, $pages);
-                header('Location: //' . AntConfig::currentConfig('baseURL') . "plugin/admin/pages/");
+                header('Location: //' . AntConfig::currentConfig('baseURL') . "admin/pages/");
                 break;
 
             default:
                 $HTMLTemplate = $this->antCMS->getThemeTemplate('admin_manage_pages_layout');
                 foreach ($pages as $key => $page) {
-                    $pages[$key]['editurl'] = '//' . AntTools::repairURL(AntConfig::currentConfig('baseURL') . "/plugin/admin/pages/edit/" . $page['functionalPagePath']);
-                    $pages[$key]['deleteurl'] = '//' . AntTools::repairURL(AntConfig::currentConfig('baseURL') . "/plugin/admin/pages/delete/" . $page['functionalPagePath']);
-                    $pages[$key]['togglevisibility'] = '//' . AntTools::repairURL(AntConfig::currentConfig('baseURL') . "/plugin/admin/pages/togglevisibility/" . $page['functionalPagePath']);
+                    $pages[$key]['editurl'] = '//' . AntTools::repairURL(AntConfig::currentConfig('baseURL') . "/admin/pages/edit/" . $page['functionalPagePath']);
+                    $pages[$key]['deleteurl'] = '//' . AntTools::repairURL(AntConfig::currentConfig('baseURL') . "/admin/pages/delete/" . $page['functionalPagePath']);
+                    $pages[$key]['togglevisibility'] = '//' . AntTools::repairURL(AntConfig::currentConfig('baseURL') . "/admin/pages/togglevisibility/" . $page['functionalPagePath']);
                     $pages[$key]['isvisable'] = $this->boolToWord($page['showInNav']);
                 }
                 $params['pages'] = $pages;
