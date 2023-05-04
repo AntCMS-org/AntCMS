0a9b067ef5a0f278fcd24834bb59d7327b94edec
Belle Aerni <belleaerni@gmail.com>
2023-05-04 14:42:36 -0700

    Move to using AntLoader

--- src/index.php
+++ src/index.php
@@ -4,8 +4,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Autoload.php';
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Constants.php';
+
+$classMapPath = __DIR__  . DIRECTORY_SEPARATOR .  'Cache'  . DIRECTORY_SEPARATOR .  'classMap.php';
+$loader = new AntCMS\AntLoader($classMapPath );
+$loader->addPrefix('AntCMS\\', __DIR__  . DIRECTORY_SEPARATOR . 'AntCMS');
+$loader->checkClassMap();
+$loader->register();
 
 use AntCMS\AntCMS;
 use AntCMS\AntConfig;
