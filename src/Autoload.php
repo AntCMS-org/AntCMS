b31e88a42864b85534a35cea65b2c44ce8283276
Belle Aerni <belleaerni@gmail.com>
2023-05-04 19:36:15 -0700

    Move to using AntLoader (#38)
    * Move to using AntLoader
    
    * Update composer lockfile
    
    I fixed the wrong classname in AntLoader
    
    * Update tests
    
    * It works!
    
    * Use AntLoader 1.0.0

--- src/Autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(static function ($class) {
-    $class = str_replace('\\', '/', $class);
-    $path = __DIR__ . '/' . $class . '.php';
-    if (is_readable($path)) {
-        require_once $path;
-    }
-});
