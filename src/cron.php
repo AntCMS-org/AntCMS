ebb0fce4924d161e42ea7a520c69dfe2eb5eff30
Belle Aerni <belleaerni@gmail.com>
2023-05-27 02:14:33 -0700

    Bump AntLoader to version 2 (#46)
    * Bump AntLoader to version 2
    
    * Whoops - I forgot about the tests
    
    * Oh yeah, cron.
    
    Note to self: turn the autoloader into an individual file that can be included by the tests / cron file so I don't need to update 3 of them manually

--- src/cron.php
+++ src/cron.php
@@ -2,8 +2,8 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 $classMapPath = __DIR__  . DIRECTORY_SEPARATOR .  'Cache'  . DIRECTORY_SEPARATOR .  'classMap.php';
-$loader = new AntCMS\AntLoader($classMapPath);
-$loader->addPrefix('AntCMS\\', __DIR__  . DIRECTORY_SEPARATOR . 'AntCMS');
+$loader = new AntCMS\AntLoader(['path' => $classMapPath]);
+$loader->addNamespace('AntCMS\\', __DIR__  . DIRECTORY_SEPARATOR . 'AntCMS');
 $loader->checkClassMap();
 $loader->register();
 
--- tests/Includes/Include.php
+++ tests/Includes/Include.php
@@ -5,7 +5,7 @@ $srcdir = $basedir . DIRECTORY_SEPARATOR . 'src';
 include_once $srcdir . DIRECTORY_SEPARATOR . 'Constants.php';
 
 $classMapPath = $srcdir  . DIRECTORY_SEPARATOR .  'Cache'  . DIRECTORY_SEPARATOR .  'classMap.php';
-$loader = new AntCMS\AntLoader($classMapPath );
-$loader->addPrefix('AntCMS\\', $srcdir  . DIRECTORY_SEPARATOR . 'AntCMS');
+$loader = new AntCMS\AntLoader(['path' => $classMapPath]);
+$loader->addNamespace('AntCMS\\', $srcdir  . DIRECTORY_SEPARATOR . 'AntCMS');
 $loader->checkClassMap();
 $loader->register();
