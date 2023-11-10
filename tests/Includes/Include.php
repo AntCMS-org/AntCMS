f29c643c507b9caec7e53b5983c6ee5629e7cf9d
Belle Aerni <belleaerni@gmail.com>
2023-11-10 06:26:36 -0800

    Update Include.php

--- tests/Includes/Include.php
+++ tests/Includes/Include.php
@@ -2,10 +2,4 @@
 $basedir = dirname(__DIR__, 2);
 $srcdir = $basedir . DIRECTORY_SEPARATOR . 'src';
 
-include_once $srcdir . DIRECTORY_SEPARATOR . 'Constants.php';
-
-$classMapPath = $srcdir  . DIRECTORY_SEPARATOR .  'Cache'  . DIRECTORY_SEPARATOR .  'classMap.php';
-$loader = new AntCMS\AntLoader(['path' => $classMapPath]);
-$loader->addNamespace('AntCMS\\', $srcdir  . DIRECTORY_SEPARATOR . 'AntCMS');
-$loader->checkClassMap();
-$loader->register();
+include_once $srcdir . DIRECTORY_SEPARATOR . 'Bootstrap.php';
