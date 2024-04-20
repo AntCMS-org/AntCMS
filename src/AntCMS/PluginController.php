d190d27c9cebe545f37570d26a36ab339c3e552b
Belle Aerni <belleaerni@gmail.com>
2024-04-20 10:36:42 -0700

    Got the robots.txt and sitemap plugins working

---
+++ src/AntCMS/PluginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AntCMS;
+
+class PluginController
+{
+    public static array $plugins = [];
+
+    /**
+     * Registers all plugin routes & sets up needed info for the plugin controller
+     */
+    public static function init(): void
+    {
+        $list = scandir(antPluginPath);
+        if (count($list) >= 2 && $list[0] === '.' && $list[1] === '..') {
+            unset($list[0]);
+            unset($list[1]);
+        }
+
+        foreach($list as $pluginName) {
+            $className = "\AntCMS\\Plugins\\$pluginName\\Controller";
+            if(class_exists($className)) {
+                new $className();
+            }
+        }
+    }
+}
---
+++ src/Plugins/Sitemap/Controller.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AntCMS\Plugins\Sitemap;
+
+use Flight;
+use AntCMS\Config;
+use AntCMS\Tools;
+use AntCMS\Pages;
+
+class Controller
+{
+    public function __construct()
+    {
+        Flight::route("GET /sitemap.xml", function (): void {
+            $protocol = Config::currentConfig('forceHTTPS') ? 'https' : Flight::request()->scheme;
+            $baseURL = Config::currentConfig('baseURL');
+
+            $pages = Pages::getPages();
+
+            if (extension_loaded('dom')) {
+                $domDocument = new \DOMDocument('1.0', 'UTF-8');
+                $domDocument->formatOutput = true;
+
+                $domElement = $domDocument->createElement('urlset');
+                $domElement->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+                $domDocument->appendChild($domElement);
+
+                $urls = [];
+                foreach ($pages as $key => $value) {
+                    $urls[$key]['url'] = $value['functionalPagePath'];
+                    $urls[$key]['lastchange'] = date('Y-m-d', filemtime($value['fullPagePath']));
+                }
+
+                foreach ($urls as $url) {
+                    $element = $domDocument->createElement('url');
+
+                    $loc = $domDocument->createElement('loc', $protocol . '://' . Tools::repairURL($baseURL . $url['url']));
+                    $element->appendChild($loc);
+
+                    $lastmod = $domDocument->createElement('lastmod', $url['lastchange']);
+                    $element->appendChild($lastmod);
+
+                    $domElement->appendChild($element);
+                }
+
+                echo $domDocument->saveXML();
+                Flight::response()->header('Content-Type', 'application/xml');
+            } else {
+                Flight::halt(503, "AntCMS is unable to generate a sitemap without having the DOM extension loadded in PHP.");
+            }
+        });
+    }
+}
