58b41aa9b65550e4b0d30cefffa60b47b2b6a966
Belle Aerni <belleaerni@gmail.com>
2024-04-20 21:58:06 -0700

    This test was for something I removed

--- tests/CMSTest.php
+++ tests/CMSTest.php
@@ -29,18 +29,6 @@ class CMSTest extends TestCase
         $this->assertIsString($result);
     }
 
-    public function testGetPageLayout(): void
-    {
-        //We need to generate the Pages.yaml file so that the nav list can be generated.
-        Pages::generatePages();
-
-        $antCMS = new AntCMS();
-        $pageLayout = $antCMS->getPageLayout();
-
-        $this->assertNotEmpty($pageLayout);
-        $this->assertIsString($pageLayout);
-    }
-
     public function testGetPage(): void
     {
         $antCMS = new AntCMS();
