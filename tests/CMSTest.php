12384d661792ccc1a3405bdb97647eae50b46281
Belle Aerni <belleaerni@gmail.com>
2024-04-22 23:17:52 -0700

    Fix the tests

--- tests/CMSTest.php
+++ tests/CMSTest.php
@@ -39,25 +39,7 @@ class CMSTest extends TestCase
         $antCMS = new AntCMS();
         $result = $antCMS->getPage('/thisdoesnotexist.md');
 
-        $this->assertEquals(false, $result);
-        $this->assertIsBool($result);
-    }
-
-    public function testGetThemeTemplate(): void
-    {
-        $antCMS = new AntCMS();
-        $result = $antCMS->getThemeTemplate();
-
-        $this->assertNotEmpty($result);
-        $this->assertIsString($result);
-    }
-
-    public function testGetThemeTemplateFallback(): void
-    {
-        $antCMS = new AntCMS();
-        $result = $antCMS->getThemeTemplate('atemplatethatjusdoesntexist');
-
-        $this->assertNotEmpty($result);
-        $this->assertIsString($result);
+        $this->assertEquals([], $result);
+        $this->assertIsArray($result);
     }
 }
