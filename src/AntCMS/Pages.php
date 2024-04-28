c3c0269dd20bb2276f6deb6da735aafdc80ece43
Belle Aerni <belleaerni@gmail.com>
2024-04-28 15:03:16 -0700

    Slight fixup for the manually specified orders

--- src/AntCMS/Pages.php
+++ src/AntCMS/Pages.php
@@ -103,7 +103,12 @@ class Pages
             if (isset($directoryMeta['pageOrder'][$a]) && isset($directoryMeta['pageOrder'][$b])) {
                 return $directoryMeta['pageOrder'][$a] > $directoryMeta['pageOrder'][$b] ? 1 : -1;
             }
-
+            if(isset($directoryMeta['pageOrder'][$a]) && !isset($directoryMeta['pageOrder'][$b])) {
+                return -1;
+            }
+            if(!isset($directoryMeta['pageOrder'][$a]) && isset($directoryMeta['pageOrder'][$b])) {
+                return 1;
+            }
             // Ensure index items come first
             if ($a === 'index') {
                 return -1;
