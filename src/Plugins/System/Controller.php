3e14a6c26a11c6946e8779b85f0f1e8f1be07df5
Belle Aerni <belleaerni@gmail.com>
2024-10-16 11:02:04 -0700

    Improved consistancy for event descriptions

--- src/Plugins/System/Controller.php
+++ src/Plugins/System/Controller.php
@@ -14,11 +14,11 @@ class Controller extends AbstractPlugin
     private array $hooks = [
         'contentHit' => 'This is fired when markdown content is accessed. The URI will be passed in the data.',
         'performanceMetricsBuilt' => 'When fired, this event contains all performance metrics AntCMS was able to collect on a request. These are more complete & accurate than the metrics shown on the bottom of the screen.',
-        'beforeApiCalled' => 'This event is fired before an API endpoint is called',
-        'afterApiCalled' => 'This event is fired after an API endpoint is called and the response is available',
+        'beforeApiCalled' => 'This event is fired before an API endpoint is called.',
+        'afterApiCalled' => 'This event is fired after an API endpoint is called and the response is available.',
         'onHookFireComplete' => 'This event is fired when others have completed. The data provided will include the hook name, timing data, and parameter read / update statistics.',
-        'onBeforeMarkdownParsed' => 'This event is fired before markdown is converted, allowing for pre-processing before the markdown is run through the parser',
-        'onAfterMarkdownParsed' => 'This is fired after markdown is converted, allowing you to modify generated markdown content',
+        'onBeforeMarkdownParsed' => 'This event is fired before markdown is converted, allowing for pre-processing before the markdown is run through the parser.',
+        'onAfterMarkdownParsed' => 'This is fired after markdown is converted, allowing you to modify generated markdown content.',
         'onAfterPluginsInit' => 'This event is fired after all plugins have been initialized.',
         'onBeforeOutputFinalized' => 'This event is fired right before the generated response is finalized (compressed) and sent to the browser. No later chances to modify the output buffer exist.',
     ];
